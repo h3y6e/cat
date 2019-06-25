aaa77ce4c1f15d95d5eeb4c113ab755e307b36b4
ShyGuy <hash.a5ebec@gmail.com>
2019-06-26 02:11:53 +0900

    rename

--- src/main.py
+++ src/main.py
@@ -1,34 +1,32 @@
 # main
-import subprocess
-from PIL import Image
 import sys
 import pyocr
 import pyocr.builders
+import subprocess
+from PIL import Image
 from googletrans import Translator
 
 def main():
     tools = pyocr.get_available_tools()
     if len(tools) == 0:
-        print("No OCR tool found")
+        print("Error: No OCR tool found")
         sys.exit(1)
     tool = tools[0]
-    print("Will use tool '%s'" % (tool.get_name()))
-    langs = tool.get_available_languages()
-    print("Available languages: %s" % ", ".join(langs))
-    lang = langs[0]
-    print("Will use lang '%s'" % (lang))
+    print("%s を使用します" % (tool.get_name()))
+    lang = tool.get_available_languages()[0]
+    print("使用言語は %s です\n" % (lang))
 
+    print("翻訳したい範囲を選択してください...\n")
     subprocess.run(["gnome-screenshot", "--area", "--file=./tmp.jpg"])
 
     origintext = tool.image_to_string(
             Image.open('tmp.jpg'),
-            lang='eng',
+            lang=lang,
             builder=pyocr.builders.TextBuilder()
-            )
+            ).replace('-\n','').replace('\n',' ')
 
-    print("eng: ", origintext)
-    text = origintext.replace('-\n','').replace('\n','')
-    text = (Translator().translate(text, dest ='ja').text)
-    print("ja_jp: ", text)
+    print("%s:\n%s\n" % (lang, origintext))
+    newtext = Translator().translate(origintext, dest ='ja').text
+    print("ja:\n%s" % (newtext))
 
 
